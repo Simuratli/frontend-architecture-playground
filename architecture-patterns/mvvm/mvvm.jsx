7614d3962cfcd1f3471d6e953cf94d5951fa357d
Simuratli <simuratli@gmail.com>
2025-09-10 11:39:11 +0400

    mvvm

---
+++ architecture-patterns/mvvm/mvvm.jsx
@@ -0,0 +1,21 @@
+// MVVM (Model-View-ViewModel) Pattern Example in React
+//ViewModel In react js is custom hook
+import { useState } from "react";
+function useTodos() {
+  const [todos, setTodos] = useState([]);
+  const addTodo = () => setTodos([...todos, `New Todo ${todos.length + 1}`]);
+
+  return { todos, addTodo };
+}
+
+// View
+function TodoView() {
+  const { todos, addTodo } = useTodos();
+
+  return (
+    <div>
+      <ul>{todos.map((t, i) => <li key={i}>{t}</li>)}</ul>
+      <button onClick={addTodo}>Add Todo</button>
+    </div>
+  );
+}
